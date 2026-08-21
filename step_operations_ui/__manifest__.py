94beec5353963409095c2b8aef96e71b01657253
Fernando Caro <fjcaroe@falabella.cl>
2026-08-21 11:02:10 -0400

    feat: add agricultural machinery freight and tracker icons

--- step_operations_ui/__manifest__.py
+++ step_operations_ui/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "Steps - Experiencia Maquinaria y Fletes",
     "summary": "Portadas y estilo agrícola para Maquinaria y Fletes",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Operations/Agriculture",
     "author": "Steps Consulting",
     "license": "LGPL-3",
     "depends": ["web", "step_agricultural_access", "step_machinery"],
     "data": [
+        "views/app_icons.xml",
         "views/dashboard_actions.xml",
         "views/machinery_views.xml",
     ],
